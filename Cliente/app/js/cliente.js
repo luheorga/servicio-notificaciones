2a6cbed5e295cb944829e33d8c663760c4db6e39
Luis Orjuela <lhorjuelag@gmail.com>
2020-05-22 16:46:28 -0500

    Se agrega signar para las notificaciones en tiempo real y las barras de progreso

--- Cliente/app/js/cliente.js
+++ Cliente/app/js/cliente.js
@@ -34,7 +34,7 @@ function hacerPeticion(metodo, url, params) {
 
 formularioAJSON = formulario => [].reduce.call(formulario.elements, (data, elemento) => {
     if (!elemento.name || !elemento.value)
-        return data;    
+        return data;
 
     data[elemento.name] = elemento.type === 'number' ? elemento.valueAsNumber : elemento.value;
 
@@ -49,22 +49,62 @@ function crearProceso() {
     const valido = formulario.reportValidity();
     if (!valido)
         return;
-    const formularioJSON = formularioAJSON(formulario);
-    hacerPeticion('POST', 'http://localhost:4200/CrearProceso', formularioJSON)
-        .then(data => alert('Proceso creado correctamente'))
-        .catch(err => alert('error en la petición', err.statusText));
+    let formularioJSON = formularioAJSON(formulario);
+    formularioJSON = { ...formularioJSON, idEmpresa: 1, idNomina: "FNX" };
+    connection.invoke("SuscribirAProceso", formularioJSON.idEmpresa, formularioJSON.idNomina, formularioJSON.tipo)
+        .then(() => {
+            console.log(`suscrito al grupo!`);
+            hacerPeticion('POST', 'http://localhost:4200/CrearProceso', formularioJSON)
+                .then(data => alert(`proceso ${formularioJSON.tipo} creado`))
+                .catch(err => alert('error en la petición', err.statusText));
+        })
+        .catch(err => console.error(err));
+
 }
 
+const listaProcesos = document.getElementById('listaProcesos');
+const progresos = {};
 
+crearProgreso = (proceso) => {
+    const div = document.createElement('div');
+    const li = document.createElement('li');
+    const txt = document.createTextNode(proceso.idProceso);
+    li.appendChild(txt);
+    li.appendChild(div);
+    listaProcesos.appendChild(li);
+    progresos[proceso.idProceso] = div;
+};
 
-// var connection = new signalR
-//     .HubConnectionBuilder()
-//     .withUrl('http://localhost:5000/notificacionHub')
-//     .withAutomaticReconnect()
-//     .build();
+notificarAvance = (proceso) => {
+    const progreso = progresos[proceso.idProceso];
 
-// Object.defineProperty(WebSocket, 'OPEN', { value: 1 });
+    if (!progreso) {
+        crearProgreso(proceso);
+        return;
+    }
 
-// connection.start()
-//     .then(() => console.log("Conectado!!"))
-//     .catch(err => console.error(err.toString()));
+    if (proceso.terminado) {
+        alert(`Proceso ${proceso.idProceso} terminado!`);        
+        const li = progreso.parentElement;
+        listaProcesos.removeChild(li);
+        delete progresos[proceso.idProceso];
+        return;
+    }    
+    progreso.style.width = `${proceso.porcentaje}%`;
+};
+
+var connection = new signalR
+    .HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Trace)
+    .withUrl('http://localhost:5000/notificacionesHub')
+    .withAutomaticReconnect()
+    .build();
+
+Object.defineProperty(WebSocket, 'OPEN', { value: 1 });
+
+connection.start()
+    .then(() => console.log("Conectado!!"))
+    .catch(err => console.error(err));
+
+connection.on('actualizarEstadoProceso', proceso => notificarAvance(proceso));
+
